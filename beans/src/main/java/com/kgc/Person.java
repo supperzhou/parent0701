b45afe6664385622f05ce88577245f406e9f8a92
supperzhou <2170597858@qq.com>
2021-10-13 19:53:19 +0800

    第三次修改

--- beans/src/main/java/com/kgc/Person.java
+++ beans/src/main/java/com/kgc/Person.java
@@ -11,6 +11,7 @@ import java.security.PrivateKey;
 public class Person {
 //阿帆发送到发送到发售
     //asdfafdasfdfafssdafasfsda
+    //gsgsfgsdgfsdfsdfggsdgfdsgdfs
     private int id;
     private String name;
     private int age;
